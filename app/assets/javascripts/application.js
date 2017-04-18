b67fdd014f2c02aa22398e85508a678da3b6f100
mcasciato <michael.casciato@icloud.com>
2017-04-18 18:36:03 -0400

    added angular, ui-router, and angular devise to application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap/dist/js/bootstrap
+//= require angular/angular
+//= require angular-ui-router/release/angular-ui-router
+//= require angular-devise
 //= require_tree .
