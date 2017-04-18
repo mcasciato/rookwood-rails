859ba78126dc1206a411757648cea42329575115
mcasciato <michael.casciato@icloud.com>
2017-04-18 18:56:11 -0400

    began creating angular routes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,4 +4,5 @@
 //= require angular/angular
 //= require angular-ui-router/release/angular-ui-router
 //= require angular-devise
+//= require angular-rails-templates
 //= require_tree .
---
+++ app/assets/javascripts/routes.js
@@ -0,0 +1,15 @@
+(function() {
+
+    'use strict';
+
+    angular
+        .module('app')
+        .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+            $stateProvider
+                .state('home', {
+                    url: '/',
+                    templateUrl: 'home/home.html',
+                    controller: 'HomeController as vm'
+                })            
+        }])
+}());
